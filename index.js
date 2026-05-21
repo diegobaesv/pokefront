0371a45e561987997ebf894c07edd8b1c0e8af2b
Diego Baes <diegobaesv@gmail.com>
2026-05-20 22:34:47 -0500

    ANTES EXAMEN PARCIAL

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const main = async () => {
+const pokemonListDiv = document.getElementById('pokemon-list');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
 
-    const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon');
+let prevUrl = null;
+let nextUrl = null;
 
-    const pokemones = await pokeResponse.json();
+const cargarPokemones = async (url) => {
 
-    console.log(pokemones);
+    const pokeResponse = await fetch(url);
+    const pokemonesData = await pokeResponse.json();
+    console.log('pokemonesData',pokemonesData)
+    const pokemones = pokemonesData.results;
+    prevUrl = pokemonesData.previous;
+    nextUrl = pokemonesData.next;
 
-    console.log(pokemones.results[0].name)
+    pokemonListDiv.innerHTML = '';
+    pokemones.forEach((pokemon)=>{
+        const splitsId = pokemon.url.split('/');
+        const id = splitsId[splitsId.length - 2];
+        pokemonListDiv.innerHTML += `
+        <div class="bg-white rounded-2xl shadow-md p-4 hover:scale-105 hover:shadow-xl transition duration-300 cursor-pointer">
+            <img class="w-28 h-28 mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">
+            <h2 class="mt-3 text-lg font-bold text-center capitalize">${pokemon.name}</h2>
+        </div>
+        `;
+    });
+
 }
 
-main();
+prevBtn.addEventListener('click', () => {
+    if(prevUrl) {
+        cargarPokemones(prevUrl);
+    }
+});
+nextBtn.addEventListener('click', () => {
+    if(nextUrl) {
+        cargarPokemones(nextUrl);
+    }
+});
+
+cargarPokemones('https://pokeapi.co/api/v2/pokemon?limit=20');
