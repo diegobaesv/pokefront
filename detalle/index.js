1b860b6f97a9a2c792aac501890cc3476f21775c
Diego Baes <diegobaesv@gmail.com>
2026-06-03 22:08:33 -0500

    update

--- detalle/index.js
+++ detalle/index.js
@@ -8,7 +8,60 @@ const cargarPokemonDetalle = async ()  => {
     const pokemonData = await pokeResponse.json();
     console.log('pokemonData',pokemonData);
 
-    
+    let typesHtml = ``;
+    for (let i = 0; i < pokemonData.types.length; i++) {
+        const type = pokemonData.types[i].type;
+        typesHtml += `<div class="bg-yellow-300 text-center rounded-sm p-1">${type.name}</div>`;
+    }
+
+    pokemonDetailDiv.innerHTML = '';
+    pokemonDetailDiv.innerHTML += `
+        <div class="bg-white rounded-2xl p-6 shadow-xl">
+                <img 
+                    class="w-80 h-80 mx-auto object-contain"
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">
+            </div>
+
+            <div class="bg-white rounded-2xl p-6 shadow-xl">
+                <div class="grid grid-cols-2 ">
+                    <h1 class="text-2xl font-bold capitalize">${pokemonData.name}</h1>
+                    <p class="text-gray-500 text-2xl text-right">#${id}</p>
+                </div>
+
+                <div class="mt-5 bg-sky-500 p-4 rounded-xl grid grid-cols-2">
+                    <div>
+                        <p class="text-sm text-white">Altura:</p>
+                        <p class="text-xl font-bold">${pokemonData.height/10}m</p>
+                    </div>
+                    <div>
+                        <p class="text-sm text-white">Peso:</p>
+                        <p class="text-xl font-bold">${pokemonData.weight/10}kg</p>
+                    </div>
+                </div>
+
+                <div class="mt-5">
+                    <p class="text-sm font-bold">Tipos:</p>
+                    <div class="grid grid-cols-3 mt-2 gap-3">
+                        ${typesHtml}
+                    </div>
+                </div>
+
+                <div class="mt-5">
+                    <p class="text-sm font-bold">Estadisticas:</p>
+                    <div class="grid grid-cols-3 mt-2 gap-3">
+                        <div class="grid grid-row-2 text-center">
+                            <p class="text-xl font-bold text-red-500 h-5 mb-1">60</p>
+                            <p class="text-xs m-0 text-gray-500">Ataque</p>
+                        </div>
+                        <div class="grid grid-row-2 text-center">
+                            <p class="text-xl font-bold text-red-500 h-5 mb-1">60</p>
+                            <p class="text-xs m-0 text-gray-500">HP</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+    `;
 }
 
 cargarPokemonDetalle();
